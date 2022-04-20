fc4ede6dcd79ab4dba83d1196a7f20fb4332f27b
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-04-20 00:54:18 -0500

    Creates Initial Thought Model

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,21 @@
-// Create the thought model
+// Import Mongoose
+const { Schema, model } = require("mongoose");
+
+// Create the Though Schema
+const ThoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+  },
+  username: {
+    type: String,
+  },
+});
+
+// Define the Thought Model
+const Thought = model("Thought", ThoughtSchema);
+
+// Export Model
+module.exports = Thought;
--- models/index.js
+++ models/index.js
@@ -1,1 +1,6 @@
+// Import Models
+const User = require("./User");
+const Thought = require("./Thought");
+
 // Export Models Created
+module.exports = { User, Thought };
