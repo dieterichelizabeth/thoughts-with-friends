849ca9c6dec1f19a62aa2e569c42c78f5e5ecc54
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-04-20 01:10:32 -0500

    Creates Initial User Model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,18 @@
-// Create the User model
+// Import Mongoose
+const { Schema, model } = require("mongoose");
+
+// Create the User Schema
+const UserSchema = new Schema({
+  username: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+});
+
+// Define the User Model
+const User = model("User", UserSchema);
+
+// Export Model
+module.exports = User;
