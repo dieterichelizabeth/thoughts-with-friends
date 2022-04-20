9489aae5664f7b8b662cc795bfdabe1302837567
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-04-20 14:27:56 -0500

    Creates GET all Thoughts and Post Thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -3,7 +3,26 @@ const { Thought } = require("../models");
 
 // Create model methods
 const thoughtController = {
-  // create a new thought
+  // Get all Thoughts
+  getAllThought(req, res) {
+    Thought.find({})
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((allThoughtData) => res.json(allThoughtData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  // Create a new Thought
+  // Expects: { "thoughtText": "This is my first thought.", "username": "Test" }
+  createThought({ body }, res) {
+    Thought.create(body)
+      .then((singleThoughtData) => res.json(singleThoughtData))
+      .catch((err) => res.status(400).json(err));
+  },
+
   // update a thought by its _id
   // remove a thought by its _id
 };
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,9 +1,14 @@
 const router = require("express").Router();
 
 // Import the Thought - Controller
-const {} = require("../../controllers/thought-controller");
+const {
+  getAllThought,
+  createThought,
+} = require("../../controllers/thought-controller");
 
 // Set up Routes
+// Route: /api/thoughts
+router.route("/").get(getAllThought).post(createThought);
 
 // Export routes
 module.exports = router;
