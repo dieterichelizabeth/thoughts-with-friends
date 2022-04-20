48861c7995abee335e79b7b8126964556ba18375
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-04-20 02:25:22 -0500

    Builds on User model

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # thoughts-with-friends
+
 A Social Networking API 🐻🦊🐼
+
+## Lessons Learned
+
+[Mongoose String Validators](https://mongoosejs.com/docs/schematypes.html#string-validators)
+
+```
+match: RegExp, creates a validator that checks if the value matches the given regular expression
+```
+
+## Documentation
+
+- [Mongoose Schema Types](https://mongoosejs.com/docs/schematypes.html)
+- [Mongoose Validators](https://mongoosejs.com/docs/validation.html)
+- [Mongoose String Validators](https://mongoosejs.com/docs/schematypes.html#string-validators)
+- [Mongoose ObjectIds Type](https://mongoosejs.com/docs/schematypes.html#objectids)
--- models/User.js
+++ models/User.js
@@ -2,14 +2,40 @@
 const { Schema, model } = require("mongoose");
 
 // Create the User Schema
-const UserSchema = new Schema({
-  username: {
-    type: String,
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
   },
-  email: {
-    type: String,
-  },
-});
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 // Define the User Model
 const User = model("User", UserSchema);
