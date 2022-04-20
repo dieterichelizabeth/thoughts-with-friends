6a9c779f2175ad0b219e62086993f1d621963efb
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-04-19 23:54:19 -0500

    Creates Basic Express Server setup

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-// Add server setup with Express and Mongoose
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware to parse req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () =>
+  console.log(`App listening on URL: http://localhost:${PORT}`)
+);
