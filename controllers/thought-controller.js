173debd66184f41d0e6cef7e1e13c69b304f334f
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-04-20 01:19:17 -0500

    Add intial controller setup

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,5 +1,12 @@
 // Import Thought models
+const { Thought } = require("../models");
 
 // Create model methods
+const thoughtController = {
+  // create a new thought
+  // update a thought by its _id
+  // remove a thought by its _id
+};
 
 // Export the controller
+module.exports = thoughtController;
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,12 @@
 // Import User models
+const { User } = require("../models");
 
 // Create model methods
+const userController = {
+  // create a new user
+  // update a user by its _id
+  // remove a user by its _id
+};
 
 // Export the controller
+module.exports = userController;
